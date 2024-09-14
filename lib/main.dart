9c6d6fc3064438585d1408e4b9f97a08099bac1f
Marian Adly <marianadly2004@gmail.com>
2024-09-14 16:34:24 +0300

    init hive and open noteBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:todo_mobile/features/home/screens/home_screen.dart';
 
 void main() async {
- 
+  await Hive.initFlutter();
+ await Hive.openBox("noteBox");
+
   runApp(const TodoApp());
 }
 
